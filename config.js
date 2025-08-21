bbc6c88f5a86ae9950e539fe40c24a6920245951
elmghariwalid234-ctrl <elmghariwalid234@gmail.com>
2025-08-21 22:24:01 +0100

    Update config.js
    dd

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ module.exports = {
         }
     },
     server: {
-        guildId: 'ID',         // ايدي السيرفر
-        broadcastRoleId: 'ID', // ايدي الرول اللي يستخدم عليها البوت
-        reportChannelId: 'ID' // ايدي الروم اللي يرسل فيه البوت التقارير
+        guildId: '1309111843221929994',         // ايدي السيرفر
+        broadcastRoleId: '1405587409189474335', // ايدي الرول اللي يستخدم عليها البوت
+        reportChannelId: '1408181143726981129' // ايدي الروم اللي يرسل فيه البوت التقارير
     },
     broadcast: {
         cooldownTime: 1000, // لا تلعب فيها
@@ -34,4 +34,5 @@ module.exports = {
         error: '#ED4245',      // Red (used for errors)
         neutral: '#5D5D5D'     // Gray (used for neutral messages)
     }
+
 };
